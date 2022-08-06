0a6618d2fcd9eecff7063aed3d3f9a522b41217d
Sachinghorpade97 <ghorpadesachin382@gmail.com>
2022-08-06 10:01:03 +0530

    FE

---
+++ script.js
@@ -0,0 +1,29 @@
+var x=true;
+var y=true;
+function unhidefun(yes)
+{
+    if(x)
+    {
+        yes.style.display="block";
+        x=false;
+    }
+    else
+    {
+        yes.style.display="none";
+        x=true;
+    }
+}
+
+function buttonchange()
+{
+    if(y==true)
+    {
+        document.getElementById("first-logo").setAttribute("class","bi bi-chevron-up");
+        y=false;
+    }
+    else
+    {
+        document.getElementById("first-logo").setAttribute("class","bi bi-chevron-down");
+        y=true;
+    }
+}
